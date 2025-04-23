8aa690edf867c8ddf531fd8827129e15c7c80ba2
ismail <ismailchafai7@gmail.com>
2025-04-23 11:02:40 +0200

    modifier main

---
+++ app/src/main/java/com/example/gencont_app/cours/CoursActivity.kt
@@ -0,0 +1,21 @@
+package com.example.gencont_app.cours
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.gencont_app.R
+
+class CoursActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_cours)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
