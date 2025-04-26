ea08dab159ac6b007e2b0cf568ad588b59b974fe
ismail <ismailchafai7@gmail.com>
2025-04-26 10:38:11 +0200

    add interface cours

---
+++ app/src/main/java/com/example/gencont_app/cours/ChapiterActivity.kt
@@ -0,0 +1,19 @@
+package com.example.gencont_app.cours
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.gencont_app.R
+
+class ChapiterActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_chapiter)
+
+
+    }
+}
