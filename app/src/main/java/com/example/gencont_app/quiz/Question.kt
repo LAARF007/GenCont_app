fbaf367eb7a679a624505bfc618a87f966bc9a26
ismail <ismailchafai7@gmail.com>
2025-04-26 21:58:24 +0200

    add quiz and score

---
+++ app/src/main/java/com/example/gencont_app/quiz/Question.kt
@@ -0,0 +1,6 @@
+package com.example.gencont_app.quiz
+
+data class Question(
+    val questionText: String,
+    val options: List<String>
+)
---
+++ app/src/main/java/com/example/gencont_app/score/ScoreActivity.kt
@@ -0,0 +1,24 @@
+package com.example.gencont_app.score
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.gencont_app.R
+
+class ScoreActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_score)
+//        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+//            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+//            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+//            insets
+//        }
+
+
+
+    }
+}
