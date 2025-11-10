5894d45718d320245ffb7c2bff6ca9d3ee53f220
Adel <adellarf002@gmail.com>
2025-11-10 16:25:04 +0100

    cmnt

--- app/src/main/java/com/example/gencont_app/cours/ChapiterActivity.kt
+++ app/src/main/java/com/example/gencont_app/cours/ChapiterActivity.kt
@@ -11,10 +11,6 @@ import com.example.gencont_app.configDB.sqlite.database.AppDatabase
 import kotlinx.coroutines.launch
 
 class ChapiterActivity : AppCompatActivity() {
-    //    private lateinit var titleTextView: MaterialTextView
-//    private lateinit var descTextView: MaterialTextView
-//    private lateinit var tvChapitreContent: MaterialTextView
-//    private lateinit var example: MaterialTextView
     private lateinit var tvCourseTitle: TextView
     private lateinit var listView: ListView
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -22,19 +18,7 @@ class ChapiterActivity : AppCompatActivity() {
         enableEdgeToEdge()
         setContentView(R.layout.activity_chapiter)
 
-
-
         tvCourseTitle = findViewById(R.id.tvCourseTitle) // À créer dans ton XML
-//        descTextView = findViewById(R.id.tvChapitreContent) // À créer dans ton XML
-//        tvChapitreContent = findViewById(R.id.tvChapitreContent) // À créer dans ton XML
-//        example = findViewById(R.id.example) // À créer dans ton XML
-
-
-//        val coursId = intent.getLongExtra("cours_id", -1L)
-//
-//        println("coursId"+coursId);
-
-
 
         val coursId = intent.getLongExtra("cours_id", -1)
         val coursTitre = intent.getStringExtra("cours_titre") ?: "-1"
@@ -53,18 +37,5 @@ class ChapiterActivity : AppCompatActivity() {
             }
         }
 
-//        if (coursId != -1L) {
-//            val db = AppDatabase.getInstance(applicationContext)
-//            CoroutineScope(Dispatchers.Main).launch {
-//                val cours = db.sectionDao().getById(coursId)
-//                cours?.let {
-//                    titleTextView.text = it.titre
-//                    descTextView.text = it.titre
-//                    tvChapitreContent.text = it.contenu
-//                    example.text = it.contenu
-//
-//                }
-//            }
-//        }
     }
 }
